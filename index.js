a67a61ccd9f6d13454466395e9bc78d86c236f7d
abhijitnr <abhijitbiswas8859@gmail.com>
2022-12-14 13:20:59 +0530

    mongodb error resolved

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 /* MONGODB CONNECT */
+mongoose.set("strictQuery", false);
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => {
